7cb72fc3344294b3791b0cbb4b2ec9f16e7ebaa9
Luiz Bills <luizbills@pm.me>
2024-08-26 10:50:11 -0300

    v3.2.2

--- wc-better-shipping-calculator-for-brazil.php
+++ wc-better-shipping-calculator-for-brazil.php
@@ -3,7 +3,7 @@
 Plugin Name: Calculadora de frete melhorada para lojas brasileiras
 Plugin URI: https://github.com/luizbills/woo-better-shipping-calculator-for-brazil
 Description: Calculadora de frete do WooCommerce otimizada para lojas brasileiras: remove dos campos de país, estado e cidade. E alguns outros ajustes.
-Version: 3.2.1
+Version: 3.2.2
 Author: Luiz Bills
 Author URI: https://luizpb.com
 Requires PHP: 7.3
@@ -12,6 +12,7 @@ License: GPLv2 or later
 License URI: http://www.gnu.org/licenses/gpl-2.0.html
 Text Domain: wc-better-shipping-calculator-for-brazil
 Domain Path: /languages
+Requires Plugins: woocommerce
 */
 
 // prevents your PHP files from being executed via direct browser access
