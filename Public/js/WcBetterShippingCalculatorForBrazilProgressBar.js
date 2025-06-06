99b3b7c2b08fcc775dd7fb7b75e514691359f791
Euzebio Batista <euzebio.batista2@gmail.com>
2025-06-06 15:35:03 +0000

    fix: message

--- Public/js/WcBetterShippingCalculatorForBrazilProgressBar.js
+++ Public/js/WcBetterShippingCalculatorForBrazilProgressBar.js
@@ -32,7 +32,7 @@
 			percent = Math.min((cartTotal / minValue) * 100, 100);
 			message = cartTotal >= minValue
 				? 'Parabéns! Você tem frete grátis!'
-				: 'Falta(m) apenas mais ' + (minValue - cartTotal).toFixed(2) + ' em compra para FRETE GRÀTIS';
+				: 'Falta(m) apenas mais R$' + (minValue - cartTotal).toFixed(2) + ' em compra para FRETE GRÀTIS';
 		}
 
 		let progressBar = document.querySelector('.wc-better-shipping-progress-bar');
