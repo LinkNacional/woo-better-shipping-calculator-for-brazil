c1fc441d891b146119d666edb1184fdf029125bd
Euzebio Batista <euzebio.batista2@gmail.com>
2026-07-01 16:22:28 -0300

    :bookmark: nova versão 4.16.1

--- Includes/WcBetterShippingCalculatorForBrazil.php
+++ Includes/WcBetterShippingCalculatorForBrazil.php
@@ -110,7 +110,7 @@ class WcBetterShippingCalculatorForBrazil
         if (defined('WC_BETTER_SHIPPING_CALCULATOR_FOR_BRAZIL_VERSION')) {
             $this->version = WC_BETTER_SHIPPING_CALCULATOR_FOR_BRAZIL_VERSION;
         } else {
-            $this->version = '4.16.0';
+            $this->version = '4.16.1';
         }
         $this->plugin_name = 'wc-better-shipping-calculator-for-brazil';
 
